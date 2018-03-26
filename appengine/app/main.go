a5b85c6973514c99984b35e10e55fbb5ce26ecea
Gosuke Miyashita <gosukenator@gmail.com>
2018-03-26 18:19:42 +0900

    Your own cetificate can be used with GAE

--- appengine/app/main.go
+++ appengine/app/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/aktsk/nolmandy/server"
 )
 
 func init() {
-	http.HandleFunc("/", server.Handler)
+	var cert *x509.Certificate
+
+	certFile, err := os.Open("cert.pem")
+	defer certFile.Close()
+
+	if err == nil {
+		certPEM, err := ioutil.ReadAll(certFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		certDER, _ := pem.Decode(certPEM)
+		cert, err = x509.ParseCertificate(certDER.Bytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	http.HandleFunc("/", server.Parse(cert))
 }
