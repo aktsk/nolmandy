fa7bc8a29d25edd42c52cc08795430b07a865509
Gosuke Miyashita <gosukenator@gmail.com>
2018-04-02 20:41:07 +0900

    Handle CERTIFICATE environment variable with GAE

--- appengine/app/main.go
+++ appengine/app/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/aktsk/nolmandy/server"
 )
@@ -28,7 +29,27 @@ func init() {
 		if err != nil {
 			log.Fatal(err)
 		}
+	} else {
+		certPEM := os.Getenv("CERTIFICATE")
+		if certPEM != "" {
+			certPEM = revertPEM(certPEM)
+			certDER, _ := pem.Decode([]byte(certPEM))
+			cert, err = x509.ParseCertificate(certDER.Bytes)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 
 	http.HandleFunc("/", server.Parse(cert))
+}
+
+// It seems GAE/Go can not handle environment variables that has
+// return code. So in YAML file, I  use ">-" to replace return
+// code with white space. This function is for reverting back PEM data
+// to original.
+func revertPEM(c string) string {
+	c = strings.Replace(c, " ", "\n", -1)
+	c = strings.Replace(c, "\nCERTIFICATE", " CERTIFICATE", -1)
+	return c
 }
